c27ff29f911277d58c16fca6dd5100683e06897c
wen527555 <wen527555@gmail.com>
2023-05-27 19:30:04 +0800

    feat:  串接product api 增加product page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 // import Banner from "../components/Banner";
 import Products from "../components/Products";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const data = useLoaderData();
+  useEffect(() => {
+    setProducts(data.data);
+  }, [data]);
   return (
     <div>
-      <Products />
+      <Products products={products} />
     </div>
   );
 };
